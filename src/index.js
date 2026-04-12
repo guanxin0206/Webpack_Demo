5ea9b6445d66279ce81b2d6c7165cb486e88a136
Sean Guan <guanxin0206@gmail.com>
2026-04-12 09:33:04 +0800

    webpack-demo initial commit css loader

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+
+function component() {
+  const element = document.createElement('div');
+  // loadash 现在使用import引入
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
