bd4bdeba1af2387dc4ee9bbeaacc496b2fbc7aec
Sean Guan <guanxin0206@gmail.com>
2026-04-14 23:51:46 +0800

    做到加载字体

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
   // loadash 现在使用import引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // 将图像添加到已经存在的 div 中。
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
